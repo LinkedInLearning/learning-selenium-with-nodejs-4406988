e636a632abe404844770d96c1239ee7a8f1b3f42
Alexandra Casapu <ale.casapu@gmail.com>
2023-06-28 11:33:15 +0200

    set 02_02 begin state

--- bread-shop-tests/test/breadShopTests.js
+++ bread-shop-tests/test/breadShopTests.js
@@ -1,29 +1,13 @@
 const { expect } = require("chai");
-const { Builder, By } = require("selenium-webdriver");
+const { Builder } = require("selenium-webdriver");
 
-describe('sandwich order', function() {
+describe('First suite', function() {
     this.timeout(5000);
 
-    it('selects the bread type', async function() {
-        //setup
+    it('First test', async function() {
         let driver = await new Builder().forBrowser('chrome').build();
-        await driver.manage().setTimeouts( {implicit: 1000} );
+        expect(1).to.equal(1);
 
-        await driver.get("http://localhost:4200/order/sandwich");
-
-        let title = await driver.getTitle();
-        expect(title).to.equal("Order a Sandwich | BreadShop");
-
-        //act 
-        let ryeBreadOptionElement = await driver.findElement(By.id("bread-type-rye"));
-        await ryeBreadOptionElement.click();
-
-        //assert
-        let selectedElement = await driver.findElement(By.className("bread-type-value"));
-        let selectedValue = await selectedElement.getText();
-        expect(selectedValue).to.equal("rye bread");
-
-        //teardown
         await driver.quit();
     });
 });
