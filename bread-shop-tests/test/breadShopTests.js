68eaa8d7c2edfbd2b7c690976435d099a802c222
Alexandra Casapu <ale.casapu@gmail.com>
2023-06-28 11:35:07 +0200

    set 01_02 begin state

--- bread-shop-tests/test/breadShopTests.js
+++ bread-shop-tests/test/breadShopTests.js
@@ -1,13 +1,7 @@
 const { expect } = require("chai");
-const { Builder } = require("selenium-webdriver");
 
 describe('First suite', function() {
-    this.timeout(5000);
-
-    it('First test', async function() {
-        let driver = await new Builder().forBrowser('chrome').build();
+    it('First test', function() {
         expect(1).to.equal(1);
-
-        await driver.quit();
     });
 });
