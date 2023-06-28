13906da4f9165dedf13aea19468f5374b375a17d
Alexandra Casapu <ale.casapu@gmail.com>
2023-06-28 11:19:36 +0200

    set 03_02 begin state

--- bread-shop-tests/test/breadShopTests.js
+++ bread-shop-tests/test/breadShopTests.js
@@ -1,4 +1,4 @@
-const { before, beforeEach, after } = require('mocha');
+const { beforeEach } = require('mocha');
 const { By, Builder, Select } = require('selenium-webdriver');
 const expect = require('chai').expect;
 
@@ -7,23 +7,15 @@ describe('sandwich order', function() {
     this.timeout(5000);
     let driver;
 
-    before(async function() {
-        driver = await new Builder().forBrowser('chrome').build();
-        await driver.manage().setTimeouts({ implicit: 1000 });
-    });
- 
     beforeEach(async function() {
         //setup
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.manage().setTimeouts({ implicit: 1000 });
         await driver.get('http://localhost:4200/order/sandwich');
         let title = await driver.getTitle();
         expect(title).to.equal("Order a Sandwich | BreadShop");
     });
 
-    after(async function() {
-        //teardown
-        await driver.quit();
-    });
- 
     it('selects the bread type', async function() {
         //act
         let ryeBreadOptionElement = await driver.findElement(By.id('bread-type-rye'));
@@ -33,6 +25,9 @@ describe('sandwich order', function() {
         let selectedElement = await driver.findElement(By.className('bread-type-value'));
         let selectedValue = await selectedElement.getText();
         expect(selectedValue).to.equal("rye bread");
+
+        //teardown
+        await driver.quit();
     });
     
     it('selects the main filling', async function() {
@@ -45,5 +40,8 @@ describe('sandwich order', function() {
         let selectedElement = await driver.findElement(By.className('main-filling-value'));
         let selectedValue = await selectedElement.getText();
         expect(selectedValue).to.equal("tofu");
+        
+        //teardown
+        await driver.quit();
     });
 });
