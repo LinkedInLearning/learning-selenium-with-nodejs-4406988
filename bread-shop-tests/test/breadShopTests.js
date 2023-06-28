d2ada83707505fc530b1ec782ee381c3cba6d5f8
Alexandra Casapu <ale.casapu@gmail.com>
2023-06-28 11:27:55 +0200

    set 03_01 begin state

--- bread-shop-tests/test/breadShopTests.js
+++ bread-shop-tests/test/breadShopTests.js
@@ -1,22 +1,17 @@
-const { beforeEach } = require('mocha');
 const { By, Builder, Select } = require('selenium-webdriver');
 const expect = require('chai').expect;
 
 describe('sandwich order', function() {
-
     this.timeout(5000);
-    let driver;
 
-    beforeEach(async function() {
+    it('selects the bread type', async function() {
         //setup
-        driver = await new Builder().forBrowser('chrome').build();
+        let driver = await new Builder().forBrowser('chrome').build();
         await driver.manage().setTimeouts({ implicit: 1000 });
         await driver.get('http://localhost:4200/order/sandwich');
         let title = await driver.getTitle();
         expect(title).to.equal("Order a Sandwich | BreadShop");
-    });
 
-    it('selects the bread type', async function() {
         //act
         let ryeBreadOptionElement = await driver.findElement(By.id('bread-type-rye'));
         await ryeBreadOptionElement.click();
@@ -31,6 +26,13 @@ describe('sandwich order', function() {
     });
     
     it('selects the main filling', async function() {
+        //setup
+        let driver = await new Builder().forBrowser('chrome').build();
+        await driver.manage().setTimeouts({ implicit: 1000 });
+        await driver.get('http://localhost:4200/order/sandwich');
+        let title = await driver.getTitle();
+        expect(title).to.equal("Order a Sandwich | BreadShop");
+
         //act
         let mainFillingElement = await driver.findElement(By.id('form-select-main-filling'));
         let select = new Select(mainFillingElement);
