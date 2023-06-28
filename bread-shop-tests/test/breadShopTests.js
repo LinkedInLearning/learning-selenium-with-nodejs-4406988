275cf082a6b2852471d8a365055c329974426d9c
Alexandra Casapu <ale.casapu@gmail.com>
2023-06-28 11:30:58 +0200

    set 02_03 begin state

--- bread-shop-tests/test/breadShopTests.js
+++ bread-shop-tests/test/breadShopTests.js
@@ -1,5 +1,5 @@
-const { By, Builder, Select } = require('selenium-webdriver');
-const expect = require('chai').expect;
+const { expect } = require("chai");
+const { Builder, By } = require("selenium-webdriver");
 
 describe('sandwich order', function() {
     this.timeout(5000);
@@ -7,42 +7,22 @@ describe('sandwich order', function() {
     it('selects the bread type', async function() {
         //setup
         let driver = await new Builder().forBrowser('chrome').build();
-        await driver.manage().setTimeouts({ implicit: 1000 });
-        await driver.get('http://localhost:4200/order/sandwich');
+        await driver.manage().setTimeouts( {implicit: 1000} );
+
+        await driver.get("http://localhost:4200/order/sandwich");
+
         let title = await driver.getTitle();
         expect(title).to.equal("Order a Sandwich | BreadShop");
 
-        //act
-        let ryeBreadOptionElement = await driver.findElement(By.id('bread-type-rye'));
+        //act 
+        let ryeBreadOptionElement = await driver.findElement(By.id("bread-type-rye"));
         await ryeBreadOptionElement.click();
 
         //assert
-        let selectedElement = await driver.findElement(By.className('bread-type-value'));
+        let selectedElement = await driver.findElement(By.className("bread-type-value"));
         let selectedValue = await selectedElement.getText();
         expect(selectedValue).to.equal("rye bread");
 
-        //teardown
-        await driver.quit();
-    });
-    
-    it('selects the main filling', async function() {
-        //setup
-        let driver = await new Builder().forBrowser('chrome').build();
-        await driver.manage().setTimeouts({ implicit: 1000 });
-        await driver.get('http://localhost:4200/order/sandwich');
-        let title = await driver.getTitle();
-        expect(title).to.equal("Order a Sandwich | BreadShop");
-
-        //act
-        let mainFillingElement = await driver.findElement(By.id('form-select-main-filling'));
-        let select = new Select(mainFillingElement);
-        await select.selectByValue('tofu');
-
-        //assert
-        let selectedElement = await driver.findElement(By.className('main-filling-value'));
-        let selectedValue = await selectedElement.getText();
-        expect(selectedValue).to.equal("tofu");
-        
         //teardown
         await driver.quit();
     });
